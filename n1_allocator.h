b6d0d8f3d3fbee14e8c5c83b717ea41fd5425f1f
Henrik Peteri <P.Henrik.Rikhard@hotmail.com>
2022-12-28 12:47:40 +0200

    Fix include

--- n1_allocator.h
+++ n1_allocator.h
@@ -13,8 +13,8 @@ typedef struct n1_Allocator{
 n1_Allocator default_allocator();
 
 #ifdef N1_ALLOCATOR_IMPLEMENTATION
+#include <stdlib.h>
 #include <string.h>
-
 n1_Allocator default_allocator(){
   n1_Allocator allocator = {.alloc = malloc,
     .free = free,
